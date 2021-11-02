5e3da3495653314e3bfb44fbded5e105e51d060a
ceaqw <2487689294@qq.com>
2021-11-02 21:50:19 +0800

    add:home_charts

---
+++ frontend/src/components/charts/common/barChart.vue
@@ -0,0 +1,104 @@
+<template>
+  <div :class="className" :style="{height:height, width:width}" />
+</template>
+
+<script>
+import * as echarts from 'echarts'
+// import theme from '@/conf/theme'
+
+export default {
+    props: {
+        className: {
+            type: String,
+            default: 'chart'
+        },
+        width: {
+            type: String,
+            default: '100%'
+        },
+        height: {
+            type: String,
+            default: '300px'
+        }
+    },
+    data() {
+        return {
+            chart: null,
+            option: {
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'shadow'
+                    }
+                },
+                legend: {},
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                xAxis: [
+                    {
+                    type: 'category',
+                    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value'
+                    }
+                ],
+                series: 
+                [
+                    {
+                        name: 'Email',
+                        type: 'bar',
+                        stack: 'Ad',
+                        emphasis: {
+                            focus: 'series'
+                        },
+                        data: [120, 132, 101, 134, 90, 230, 210]
+                    },
+                    {
+                        name: 'Union Ads',
+                        type: 'bar',
+                        stack: 'Ad',
+                        emphasis: {
+                            focus: 'series'
+                        },
+                        data: [220, 182, 191, 234, 290, 330, 310]
+                    },
+                    {
+                        name: 'Video Ads',
+                        type: 'bar',
+                        stack: 'Ad',
+                        emphasis: {
+                            focus: 'series'
+                        },
+                        data: [150, 232, 201, 154, 190, 330, 410]
+                    },
+                ]
+            },
+        }
+    },
+    mounted() {
+        this.$nextTick(() => {
+            this.initChart()
+        })
+    },
+    beforeDestroy() {
+        if (!this.chart) {
+            return
+        }
+        this.chart.dispose()
+        this.chart = null
+    },
+    methods: {
+        initChart() {
+            this.chart = echarts.init(this.$el)
+            this.chart.setOption(this.option)
+        }
+    }
+}
+</script>
--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -2,26 +2,89 @@
     <div class="home">
         <!-- 筛选条件布局 -->
         <Filter /> 
+        <div class="row">
+            <div class="title">总览图表</div>
+            <el-divider></el-divider>
+            <el-row>
+                <el-col :span="10">
+                    <PieChart />
+                </el-col>
+                <!-- <el-col :span="1">
+                    <el-divider direction="vertical" style="height: 100%"></el-divider>
+                </el-col> -->
+                <el-col :span="14">
+                    <BarChart />
+                </el-col>
+            </el-row>
+        </div>
+        <div class="row">
+            <div class="title">Analyse监控</div>
+            <el-divider></el-divider>
+            <el-row>
+                <el-col :span="10">
+                    <PieChart />
+                </el-col>
+                <!-- <el-col :span="1">
+                    <el-divider direction="vertical" style="height: 100%"></el-divider>
+                </el-col> -->
+                <el-col :span="14">
+                    <BarChart />
+                </el-col>
+            </el-row>
+        </div>
         <el-row>
-            <line-chart :chart-data="lineChartData" />
+            <el-col :span="8">
+                <div class="row">
+                    <div class="title">Rakuten</div>
+                    <el-divider></el-divider>
+                    <el-row>
+                        <PieChart />
+                    </el-row>
+                    <el-divider></el-divider>
+                    <el-row>
+                        <BarChart />
+                    </el-row>
+                </div>
+            </el-col>
+            <el-col :span="1"></el-col>
+            <el-col :span="7">
+                <div class="row">
+                    <div class="title">Yahoo</div>
+                    <el-divider></el-divider>
+                    <el-row>
+                        <PieChart />
+                    </el-row>
+                    <el-divider></el-divider>
+                    <el-row>
+                        <BarChart />
+                    </el-row>
+                </div>
+            </el-col>
+            <el-col :span="1"></el-col>
+            <el-col :span="7">
+                <div class="row">
+                    <div class="title">Amazon</div>
+                    <el-divider></el-divider>
+                    <el-row>
+                        <PieChart />
+                    </el-row>
+                    <el-divider></el-divider>
+                    <el-row>
+                        <BarChart />
+                    </el-row>
+                </div>
+            </el-col>
         </el-row>
-        <el-row>
-            <el-col :xs="24" :sm="24" :lg="8">
-                <div class="chart-wrapper">
-                    <raddar-chart />
-                </div>
-            </el-col>
-            <el-col :xs="24" :sm="24" :lg="8">
-                <div class="chart-wrapper">
-                    <pie-chart />
-                </div>
-            </el-col>
-            <el-col :xs="24" :sm="24" :lg="8">
-                <div class="chart-wrapper">
-                    <bar-chart />
-                </div>
-            </el-col>
-        </el-row>
+        <div class="row">
+            <div class="title">TOP ERROR</div>
+            <el-divider></el-divider>
+            <el-row>
+                <el-col :span="10">
+                    <PieChart />
+                </el-col>
+                <el-col :span="14"></el-col>
+            </el-row>
+        </div>
   </div>
 </template>
 
@@ -29,10 +92,8 @@
 import theme from '@/conf/theme'
 import Filter from '@/components/filter'
 
-import LineChart from './dashboard/LineChart'
-import RaddarChart from './dashboard/RaddarChart'
-import PieChart from './dashboard/PieChart'
-import BarChart from './dashboard/BarChart'
+import PieChart from '@/components/charts/common/pieChart'
+import BarChart from '@/components/charts/common/barChart'
 
 const lineChartData = {
   newVisitis: {
@@ -56,15 +117,16 @@ const lineChartData = {
 export default {
     components: {
         Filter,
-        LineChart,
-        RaddarChart,
         PieChart,
-        BarChart
+        BarChart,
     },
     data() {
         return {
             lineChartData: lineChartData.newVisitis,
-            theme
+            theme,
+            commonInfos: {
+                title: ['all_platform', 'all_project']
+            }
         }
     },
     methods: {
@@ -87,11 +149,24 @@ export default {
     }
 }
 
-.home .el-row {
-    padding: 10px;
+.home .row{
+    padding: 0 20px;
     margin-bottom: 15px;
     border-radius: 10px;
     box-shadow: 0px 3px 7px #D3DCE6;
     background-color: #F2F5F5;
+    text-align: center;
+}
+
+.home .title {
+    font-size: 130%;
+    text-align: left;
+    margin: 10px 5px 5px;
+    font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
+    'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
+}
+
+.home .el-divider {
+    margin: 5px;
 }
 </style>
